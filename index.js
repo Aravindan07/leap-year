6127b90cd9d4b31970e4f27b145733ced92e633a
Aravindan R <aravindanravi33@gmail.com>
2020-12-16 08:41:26 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const readlineSync = require('readline-sync');
+const chalk = require('chalk');
+
+console.log(chalk.yellowBright("This is a program to find whether you are born in a leap year or not.\n"));
+
+let name = readlineSync.question("May I have your name \n");
+
+console.log(chalk.redBright("Please enter your date of birth in the specified format.\n"));
+
+function calculateLeapYear(){
+  let birthDate = readlineSync.question("\nEnter your date of birth in dd/mm/yy format E.g: 27/07/1999\n");
+  let birthYear = birthDate.split('/')[2];
+  if(!birthYear){
+    console.log(chalk.redBright("Please enter the date in the above specified format"));
+    return;
+  }
+  if((birthYear % 4 === 0 ) && (birthYear % 100 !=0) || (birthYear % 4 ===0)){
+    console.log(chalk.blueBright(`\nHey, ${name} you born in LEAP YEAR\n`));
+    console.log(chalk.yellowBright(`\n You can share it on social media that you are born in a leap year.`));
+    return;
+  }
+  else{
+    console.log(chalk.red(`\n${name} you didn't born in the leap year`));
+    return;
+  }
+}
+
+calculateLeapYear();
